1dd7e9e62554139936851149b8821624fa1d5a5a
Israel <israelalisoso1@gmail.com>
2021-07-04 22:42:18 -0700

    'test'

---
+++ apps/my-portfolio/pages/index.tsx
@@ -0,0 +1,16 @@
+import styles from './index.module.scss';
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.scss file.
+   */
+  return (
+    <div className={styles.page}>
+            Nextjs Works Correctly
+    </div>
+  );
+}
+
+export default Index;
